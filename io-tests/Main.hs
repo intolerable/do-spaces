814212ce54588b51626ba027c8a05bb6cf0508b9
Rory Tyler Hayford <rory.hayford@protonmail.com>
2021-04-26 20:48:18 +0700

    IO tests for object and multipart CRUD

--- io-tests/Main.hs
+++ io-tests/Main.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
@@ -37,62 +36,152 @@ module Main where
 
 import           Conduit
                  ( (.|)
-                 , MonadThrow(throwM)
                  , runConduitRes
                  , sinkList
                  , sourceFile
+                 , sourceLazy
                  )
 
-import           Control.Exception       ( throwIO )
-import           Control.Monad.Catch     ( MonadCatch(catch) )
-import           Control.Monad.IO.Class  ( MonadIO(liftIO) )
+import           Control.Exception          ( bracket, throwIO )
+import           Control.Monad              ( void )
+import           Control.Monad.Catch        ( MonadCatch(catch)
+                                            , MonadThrow(throwM)
+                                            )
+import           Control.Monad.IO.Class     ( MonadIO(liftIO) )
 
-import qualified Data.ByteString.Char8   as C
-import           Data.Coerce             ( coerce )
-import qualified Data.Conduit.Binary     as CB
-import           Data.Generics.Labels    ()
-import qualified Data.Text               as T
-import qualified Data.Text.Encoding      as T
-import           Data.Time.Clock.POSIX   ( getPOSIXTime )
+import qualified Data.ByteString.Char8      as C
+import qualified Data.ByteString.Lazy.Char8 as LBC
+import           Data.Coerce                ( coerce )
+import qualified Data.Conduit.Binary        as CB
+import           Data.Generics.Labels       ()
+import qualified Data.Text                  as T
+import           Data.Text                  ( Text )
+import qualified Data.Text.Encoding         as T
+import           Data.Time.Clock.POSIX      ( getPOSIXTime )
 
 import           Lens.Micro
 
 import           Network.DO.Spaces
-import           Network.DO.Spaces.Types ( SpacesResponse )
-import           Network.DO.Spaces.Utils ( slugToRegion )
-import qualified Network.HTTP.Types      as H
+import           Network.DO.Spaces.Utils    ( slugToRegion )
+import qualified Network.HTTP.Types         as H
 
-import           System.Time.Extra       ( sleep )
+import           System.Time.Extra          ( sleep )
 
 import           Test.Hspec
 
 main :: IO ()
-main = bucketCrud
+main = sequence_ [ bucketCreateDelete
+                 , objectCreateDelete
+                 , objectActions
+                 , multipart
+                 ]
 
-bucketCrud :: IO ()
-bucketCrud = do
+objectActions :: IO ()
+objectActions = do
+    (bucket, spaces) <- readConf
+    hspec . around withTestObject $ do
+        describe "Network.DO.Spaces.Actions.GetObjectInfo"
+            . it "retrieves object information"
+            $ \object -> do
+                info <- retry404 20 . runSpaces spaces
+                    $ getObjectInfo bucket object
+                (info ^. #result . #contentType) `shouldBe` "text/plain"
+                (info ^. #result . #contentLength) `shouldBe` 18
+
+        describe "Network.DO.Spaces.Actions.CopyObject"
+            . it "copies an existing object to a new object"
+            $ \object -> do
+                destObject <- nameWithEpoch mkObject "test-object-copy-"
+                copied <- runSpaces spaces
+                    $ copyObjectWithin bucket object destObject
+                getStatus copied `shouldBe` Just 200
+                deleted <- retry404 20 . runSpaces spaces
+                    $ deleteObject bucket destObject
+                getStatus deleted `shouldBe` Just 204
+
+        describe "Network.DO.Spaces.Actions.GetObject"
+            . it "retrieves object data"
+            $ \object -> do
+                gotten <- runSpaces spaces $ getObject bucket object
+                (gotten ^. #result . #objectData)
+                    `shouldBe` "hello from haskell"
+  where
+    withTestObject = bracket uploadTestObject deleteTestObject
+
+    uploadTestObject = do
+        (bucket, spaces) <- readConf
+        object <- nameWithEpoch mkObject "test-object-"
+        void . runSpaces spaces
+            $ uploadObject (Just "text/plain") bucket object body
+        return object
+      where
+        body = sourceLazy "hello from haskell"
+
+    deleteTestObject object = do
+        (bucket, spaces) <- readConf
+        void . runSpaces spaces $ deleteObject bucket object
+
+multipart :: IO ()
+multipart = do
+    (bucket, spaces) <- readConf
+    object <- nameWithEpoch mkObject "test-multipart-"
+    hspec
+        . after_ (cleanup spaces bucket object)
+        . describe "Network.DO.Spaces.multipartObject"
+        . it "uploads a multipart object"
+        $ do
+            mp <- runSpaces spaces
+                $ multipartObject Nothing bucket object 5242880 body
+            getStatus mp `shouldBe` Just 200
+            (mp ^. #result . #object) `shouldBe` object
+  where
+    cleanup spaces bucket object =
+        void . retry404 20 . runSpaces spaces $ deleteObject bucket object
+
+    body = sourceLazy $ LBC.replicate 10485760 'f'
+
+objectCreateDelete :: IO ()
+objectCreateDelete = do
+    (bucket, spaces) <- readConf
+    object <- nameWithEpoch mkObject "test-object-"
+    hspec
+        . context "Network.DO.Spaces.Actions Object CRUD"
+        . it "creates, reads, and deletes a new Object"
+        $ do
+            created
+                <- runSpaces spaces $ uploadObject Nothing bucket object body
+            getStatus created `shouldBe` Just 200
+
+            deleted
+                <- retry404 20 . runSpaces spaces $ deleteObject bucket object
+            getStatus deleted `shouldBe` Just 204
+            (deleted ^. #result) `shouldBe` ()
+
+            pending
+  where
+    body = sourceLazy "hello from haskell"
+
+bucketCreateDelete :: IO ()
+bucketCreateDelete = do
     (_, spaces) <- readConf
-    !bucket <- epochBucket
+    bucket <- nameWithEpoch mkBucket "do-spaces-test-"
     hspec
         . context "Network.DO.Spaces.Actions Bucket CRUD"
         . it "creates, reads, and deletes a new bucket"
         $ do
             created <- runSpaces spaces $ createBucket bucket Nothing Nothing
-            (created ^? #metadata . _Just . #status . to H.statusCode)
-                `shouldBe` Just 200
-            (created ^. #value) `shouldBe` ()
+            getStatus created `shouldBe` Just 200
+            (created ^. #result) `shouldBe` ()
 
             location
                 <- retry404 20 . runSpaces spaces $ getBucketLocation bucket
-            (location ^? #metadata . _Just . #status . to H.statusCode)
-                `shouldBe` Just 200
-            (location ^. #value . #locationConstraint)
+            getStatus location `shouldBe` Just 200
+            (location ^. #result . #locationConstraint)
                 `shouldBe` (spaces ^. #region)
 
             deleted <- retry404 20 . runSpaces spaces $ deleteBucket bucket
-            (deleted ^? #metadata . _Just . #status . to H.statusCode)
-                `shouldBe` Just 204
-            (deleted ^. #value) `shouldBe` ()
+            getStatus deleted `shouldBe` Just 204
+            (deleted ^. #result) `shouldBe` ()
 
 -- A crude (yet effective) mechanism to ensure that 404s are retried (up to a
 -- limit), necessary when creating a new Bucket for testing
@@ -111,23 +200,24 @@ retry404 maxRetries action = loop 0
 
     catch404 _ e                  = throwM e
 
--- Makes a Bucket by appending the current epoch time to a base name; this
--- provides some mechanism to avoid name clashes when creating new buckets,
--- which must be unique across all users within a given region
-epochBucket :: IO Bucket
-epochBucket =
-    mkBucket . ("do-spaces-test-" <>) . T.pack . show @Integer . round
-    =<< getPOSIXTime
+-- Makes a Bucket or Object name by appending the current epoch time to a base name;
+-- this provides some mechanism to avoid name clashes when creating new buckets,
+-- which must be unique across all users within a given region. It also helps ensure
+-- that new Objects created during testing won't overwrite existing ones in the
+-- default Bucket provided in the credentials configuration
+nameWithEpoch :: MonadIO m => (Text -> m a) -> Text -> m a
+nameWithEpoch f base = f . (base <>) . T.pack . show @Integer . round
+    =<< liftIO getPOSIXTime
 
 readConf :: IO (Bucket, Spaces)
 readConf = do
     contents <- runConduitRes
         $ sourceFile "./io-tests/creds.conf" .| CB.lines .| sinkList
-    case contents of
+    case getVal <$> contents of
         [ a, s, r, b ] -> do
-            let access = coerce $ getVal a
-                secret = coerce $ getVal s
-            region <- slugToRegion . T.decodeUtf8 $ getVal r
+            let access = coerce a
+                secret = coerce s
+            region <- slugToRegion $ T.decodeUtf8 r
             bucket <- mkBucket $ T.decodeUtf8 b
             spaces <- newSpaces region (Explicit access secret)
             return (bucket, spaces)
@@ -141,3 +231,6 @@ readConf = do
                       ]
   where
     getVal = snd . C.splitAt 7
+
+getStatus :: SpacesResponse a -> Maybe Int
+getStatus = (^? #metadata . _Just . #status . to H.statusCode)
